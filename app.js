c4180cf5aa089559db5ebd5f5ae805d49fc0774a
Henrique Ambiel <henriqueambiel@gmail.com>
2024-09-23 03:24:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-//Texto exibido na página e variaveis
+//Texto exibido na página
 alert('Bem vindo ao game do número secreto');
+
+//Variáveis
 let numeroSecreto = 23;
 console.log(numeroSecreto);
-let palpite = prompt('Escolha um número entre 1 e 30');
+let palpite;
+let tentativas = 1;
 
-// Se o chut for = ao número secreto, a pessoa acertou o número
-if(palpite == numeroSecreto ){
-    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto}`);
-} else{
-    alert('Você ERRRRRRROUUUU');
+//Enquanto palpite não for igual ao número secreto, o site continuará perguntando qual número é
+while(palpite != numeroSecreto){
+    palpite = prompt('Escolha um número entre 1 e 30');
+    // Se o chute for = ao número secreto, a pessoa acertou o número, se não, será exibido o quão próximo está o número
+    if(palpite == numeroSecreto ){
+    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    }   else{
+        if(numeroSecreto > palpite){
+        alert(`O número secreto é maior do que o ${palpite}`);
+        } else {
+        alert(`O número secreto é menor do que o ${palpite}`);
+        }
+        //Adiciona o número de tentativas a cada erro
+        tentativas++;
+    }
 }

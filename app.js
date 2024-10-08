9f99829b0f7022840b5fd805c492a668b96cddf8
Henrique Ambiel <henriqueambiel@gmail.com>
2024-10-08 02:03:40 -0300

    Finalizando

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 alert('Bem vindo ao game do número secreto');
 
 //Variáveis
-let numeroSecreto = 23;
+let numeroMaximo = 300;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); //Gera um número secreto aleatório e inteiro
 console.log(numeroSecreto);
 let palpite;
 let tentativas = 1;
 
 //Enquanto palpite não for igual ao número secreto, o site continuará perguntando qual número é
 while(palpite != numeroSecreto){
-    palpite = prompt('Escolha um número entre 1 e 30');
+    palpite = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // Se o chute for = ao número secreto, a pessoa acertou o número, se não, será exibido o quão próximo está o número
     if(palpite == numeroSecreto ){
-    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break; //Se a condição for verdadeira, o looping é interrompido na hora
     }   else{
         if(numeroSecreto > palpite){
         alert(`O número secreto é maior do que o ${palpite}`);
@@ -23,3 +24,7 @@ while(palpite != numeroSecreto){
         tentativas++;
     }
 }
+
+//Operador ternário verifica se a quantidade de tentativas é maior que 1 para realizar a concordância do texto
+let quantasTentativas = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${quantasTentativas}`);
